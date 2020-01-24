302951e7dc52f744f98c79bd008015665ddb9515
Ron <ronliu2k@gmail.com>
2020-01-24 13:53:10 +1100

    finished parse timespan

--- src/utils/date-time.ts
+++ src/utils/date-time.ts
@@ -1,5 +1,6 @@
 import { format } from 'date-fns'
 import { Time, FunctionResult, CreateTimeSpanParams } from '../types'
+import { match } from 'ramda'
 
 export const createTime = (hours: number, minutes: number = 0): Time => {
   const date: [number, number, number] = [
@@ -50,11 +51,26 @@ export const createDate = (y: number, m: number, d: number): Date =>
 export const parseCreateTimeSpanParams = (
   str: string
 ): FunctionResult<CreateTimeSpanParams> => {
+  const rex = /^[\s]*(?<startHour>\d{1,2})([\s]*\:[\s]*(?<startMin>\d{1,2}))?[\s]*to[\s]*(?<endHour>\d{1,2})([\s]*\:[\s]*(?<endMin>\d{1,2}))?([\s]*[\,][\s]*(?<breaksHour>\d{1,2})([\s]*\:[\s]*(?<breaksMin>\d{1,2}))?)?([\s]*[\,][\s]*(?<comment>[\s|\S]*))?/
+  const matchs = str.match(rex)?.groups
+  if (!matchs)
+    return {
+      errors: [
+        'The format is not right, should be: start to end[, break[, comment]], like: 8:00 to 16:30, 00:30',
+      ],
+    }
   return {
     result: {
-      start: createTime(8, 0),
-      end: createTime(16, 30),
-      breaks: createTime(0, 30),
+      start: createTime(
+        parseInt(matchs.startHour),
+        parseInt(matchs.startMin ?? '0')
+      ),
+      end: createTime(parseInt(matchs.endHour), parseInt(matchs.endMin ?? '0')),
+      breaks: createTime(
+        parseInt(matchs.breaksHour ?? '0'),
+        parseInt(matchs.breaksMin ?? '0')
+      ),
+      comment: matchs.comment,
     },
   }
 }
